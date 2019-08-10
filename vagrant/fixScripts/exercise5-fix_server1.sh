3e0c6128b02f3d514445c0ef15aafd203a791a74
Idan <idanhaim@gmail.com>
2019-08-10 23:28:23 +0300

    Exercise 5 fix server1 - ssh password-less and without host key checking to server2

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+server2_ssh_public_key=`ssh-keyscan 192.168.100.11`
+echo $server2_ssh_public_key >> /home/vagrant/.ssh/known_hosts
+
+sshpass_exist=`command -v sshpass`
+if [ -z $sshpass_exist ]
+then
+        sudo apt-get install sshpass
+fi
+
+if [ ! -f /home/vagrant/.ssh/id_rsa ]
+then
+       ssh-keygen -q -N "" -f /home/vagrant/.ssh/id_rsa
+fi
+sshpass -p 'vagrant' ssh-copy-id vagrant@192.168.100.11
