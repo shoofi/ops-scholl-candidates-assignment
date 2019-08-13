c80f13dfb15f6bfbda7d864165117e5e62e3d2b5
Idan <idanhaim@gmail.com>
2019-08-14 00:03:09 +0300

    Exercise 5 server1 - Adding server2 to root's known_hosts and chown id_rsa* files to vagrant

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
-server2_ssh_public_key=`ssh-keyscan 192.168.100.11`
-echo $server2_ssh_public_key >> /home/vagrant/.ssh/known_hosts
+sudo -- sh -c -e "ssh-keyscan 192.168.100.11 >> /home/vagrant/.ssh/known_hosts"
+sudo -- sh -c -e "ssh-keyscan 192.168.100.11 >> /root/.ssh/known_hosts"
 
 sshpass_exist=`command -v sshpass`
 if [ -z $sshpass_exist ]
@@ -13,4 +13,7 @@ if [ ! -f /home/vagrant/.ssh/id_rsa ]
 then
        ssh-keygen -q -N "" -f /home/vagrant/.ssh/id_rsa
 fi
-sshpass -p 'vagrant' ssh-copy-id vagrant@192.168.100.11
+sudo chown vagrant:vagrant /home/vagrant/.ssh/id_rsa*
+
+sshpass -p 'vagrant' ssh-copy-id -i .ssh/id_rsa.pub vagrant@192.168.100.11
+sshpass -p 'vagrant' ssh-copy-id -i .ssh/id_rsa.pub root@192.168.100.11
